4cf2e9ccdc3090b8c68242aa15e2ac44d3165389
Martin <huim13a@gmail.com>
2022-12-28 00:59:15 +0800

    update

--- src/components/cube.js
+++ src/components/cube.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useTransition } from 'react';
 import test1 from '../materials/images/1.jpg'
 import test2 from '../materials/images/2.jpg'
 import { useLoader } from '@react-three/fiber'
@@ -11,7 +11,6 @@ export default function Cube(){
     return (
         <mesh>
             <boxBufferGeometry attach="geometry"/>
-            {/*<meshLambertMaterial attach="material" color="white" map={loader1}/>*/}
             <meshLambertMaterial attach="material-0" color="0xffffff" />
             <meshLambertMaterial attach="material-1" color="0xffffff" />
             <meshLambertMaterial attach="material-2" color="0xffffff" map={loader2}/>
