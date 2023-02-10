5da2fdaae1709ff929e7abed0bd664537838cd8c
Martin Hui <huim13a@gmail.com>
2023-02-10 16:22:35 +0800

    update

--- src/tutorials/tut_2.js
+++ src/tutorials/tut_2.js
@@ -25,6 +25,12 @@ export default function Tut_2() {
         width: "1000px",
         height: "500px"
     };
+    const paragraphTextNoMargin = {
+        textAlign: "justify",
+        color: "#cccccc",
+        marginRight: "20%",
+        lineHeight: 2
+    };
     return (
         <>
             <motion.div
@@ -35,11 +41,67 @@ export default function Tut_2() {
                 <h1 style={titleText}>
                     {t("tutorial_2.title")}
                 </h1>
+                <h2 style={subTitleText}>
+                    {t("tutorial_2.subTitle_1")}
+                </h2>
+                <h3 style={paragraphTextNoMargin}>
+                    {t("tutorial_2.paragraph_1_1")}
+                </h3>
+                <br/>
+                <h2 style={subTitleText}>
+                    {t("tutorial_2.subTitle_3")}
+                </h2>
+                <h3 style={paragraphTextNoMargin}>
+                    {t("tutorial_2.paragraph_3_1")}
+                </h3>
+                <h3 style={paragraphTextNoMargin}>
+                    {t("tutorial_2.paragraph_3_2")}
+                </h3>
+                <h3 style={paragraphTextNoMargin}>
+                    {t("tutorial_2.paragraph_3_3")}
+                </h3>
+                <h3 style={paragraphTextNoMargin}>
+                    {t("tutorial_2.paragraph_3_4")}
+                </h3>
+                <h3 style={paragraphTextNoMargin}>
+                    {t("tutorial_2.paragraph_3_5")}
+                </h3>
+                <br/>
+                <h2 style={subTitleText}>
+                    {t("tutorial_2.subTitle_2")}
+                </h2>
+                <h3 style={paragraphTextNoMargin}>
+                    {t("tutorial_2.paragraph_2_1")}
+                </h3>
+                <br/>
                 <div>
                     <Paper style={paperStyle} elevation={24} square={false}>
                         <DragStructure/>
                     </Paper>
                 </div>
+                <br/>
+                <br/>
+                <h3 style={paragraphTextNoMargin}>
+                    {t("tutorial_2.paragraph_2_2")}
+                </h3>
+                <h3 style={paragraphTextNoMargin}>
+                    {t("tutorial_2.paragraph_2_3")}
+                </h3>
+                <h3 style={paragraphTextNoMargin}>
+                    {t("tutorial_2.paragraph_2_3_1")}
+                </h3>
+                <h3 style={paragraphTextNoMargin}>
+                    {t("tutorial_2.paragraph_2_4")}
+                </h3>
+                <h3 style={paragraphTextNoMargin}>
+                    {t("tutorial_2.paragraph_2_4_1")}
+                </h3>
+                <h3 style={paragraphTextNoMargin}>
+                    {t("tutorial_2.paragraph_2_5")}
+                </h3>
+                <h3 style={paragraphTextNoMargin}>
+                    {t("tutorial_2.paragraph_2_5_1")}
+                </h3>
             </motion.div>
         </>
     )
