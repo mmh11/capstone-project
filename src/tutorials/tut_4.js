05aa8d375b9911bff5cb243d26db7c270fbea57e
Martin Hui <huim13a@gmail.com>
2023-03-05 00:02:02 +0800

    update

--- src/tutorials/tut_4.js
+++ src/tutorials/tut_4.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useTranslation } from 'react-i18next';
 import { motion } from "framer-motion";
+import trans1EN from "../materials/images/trans1EN.png"
+import trans1TC from "../materials/images/trans1TC.png"
+import trans2EN from "../materials/images/trans2EN.png"
+import trans2TC from "../materials/images/trans2TC.png"
+import CustomTextField from '../components/customTextField';
 
 export default function Tut_4() {
     const { t, i18n } = useTranslation();
@@ -18,6 +23,81 @@ export default function Tut_4() {
         marginRight: "20%",
         lineHeight: 2
     };
+    const flexDivStyle = {
+        display: "flex",
+        color: "#cccccc"
+    }
+    const trans1DivStyle = i18n.language === "EN" 
+    ? {
+        backgroundImage: `url(${trans1EN})`,
+        display: "inline-flex",
+        height: "362px",
+        width: "722px",
+    }
+    : {
+        backgroundImage: `url(${trans1TC})`,
+        display: "inline-flex",
+        height: "362px",
+        width: "722px",
+    }
+    const trans2DivStyle = i18n.language === "EN" 
+    ? {
+        backgroundImage: `url(${trans2EN})`,
+        display: "inline-flex",
+        height: "362px",
+        width: "722px",
+    }
+    : {
+        backgroundImage: `url(${trans2TC})`,
+        display: "inline-flex",
+        height: "362px",
+        width: "722px",
+    }
+    const trans1text1Style = {
+        position: "absolute",
+        color: "#cccccc",
+        marginLeft: "160px",
+        marginTop: "195px"
+    }
+    const trans1text2Style = {
+        position: "absolute",
+        color: "#cccccc",
+        marginLeft: "605px",
+        marginTop: "66px"
+    }
+    const trans1text3Style = {
+        position: "absolute",
+        color: "#cccccc",
+        marginLeft: "605px",
+        marginTop: "305px"
+    }
+
+    const trans2text1Style = {
+        position: "absolute",
+        color: "#cccccc",
+        marginLeft: "160px",
+        marginTop: "66px"
+    }
+    const trans2text2Style = {
+        position: "absolute",
+        color: "#cccccc",
+        marginLeft: "160px",
+        marginTop: "306px"
+    }
+    const trans2text3Style = {
+        position: "absolute",
+        color: "#cccccc",
+        marginLeft: "605px",
+        marginTop: "196px"
+    }
+    const validationStyle = {
+        color: "red"
+    }
+    const [trans1Text1, setTrans1Text1] = useState(0)
+    const [trans1Text2, setTrans1Text2] = useState(0)
+    const [trans2Text1, setTrans2Text1] = useState(0)
+    const [trans2Text2, setTrans2Text2] = useState(0)
+    const [valid, setValid] = useState(true)
     return (
         <>
             <motion.div
@@ -60,13 +140,144 @@ export default function Tut_4() {
                 <h3 style={paragraphTextNoMargin}>
                     {t("tutorial_4.paragraph_2_5_1")}
                 </h3>
+
+                <br/>
+                <div style={flexDivStyle}>
+                    <h3>
+                        {t("tutorial_4.trans1Assume_1")+"\u00a0"}
+                    </h3>
+                    <CustomTextField
+                        onChange = {
+                            (e) => {
+                                if (e.length === 0){
+                                    setTrans1Text1(0)
+                                    if (trans1Text2 !== 0){
+                                        setValid(false)
+                                    }
+                                } else {
+                                    setTrans1Text1(parseInt(e)) 
+                                    if (parseInt(e) - trans1Text2 < 0){
+                                        setValid(false)
+                                    } else {
+                                        setValid(true)
+                                    }
+                                }
+                            }
+                        }
+                    />
+                    <h3>
+                        {"\u00a0"+t("tutorial_4.dollars")}
+                    </h3>
+                </div>
+                <div style={flexDivStyle}>
+                    <h3>
+                        {t("tutorial_4.trans1Assume_2")+"\u00a0"}
+                    </h3>
+                    <CustomTextField 
+                        onChange = {
+                            (e) => {
+                                if (e.length === 0){
+                                    setTrans1Text2(0)
+                                    if (trans1Text1 - parseInt(e) < 0){
+                                        setValid(false)
+                                    } else {
+                                        setValid(true)
+                                    }
+                                } else {
+                                    setTrans1Text2(parseInt(e)) 
+                                    if (trans1Text1 - parseInt(e) < 0){
+                                        setValid(false)
+                                    } else {
+                                        setValid(true)
+                                    }
+                                }
+                            }
+                        }
+                    />
+                    <h3>
+                        {"\u00a0"+t("tutorial_4.dollars")}
+                    </h3>
+                </div>
+
+                <br/>
+                <div style={trans1DivStyle}>
+                    <h3 style={trans1text1Style}>
+                        {trans1Text1}
+                    </h3>
+                    <h3 style={trans1text2Style}>
+                        {trans1Text2}
+                    </h3>
+                    <h3 style={trans1text3Style}>
+                        {parseInt(trans1Text1)-parseInt(trans1Text2)}
+                    </h3>
+                </div>
+                {!valid && 
+                    <h3 style={validationStyle}>
+                        {t("tutorial_4.valid1")}
+                    </h3>
+                }
+                <br/><br/><br/><br/><br/><br/>
+
                 <h3 style={paragraphTextNoMargin}>
                     {t("tutorial_4.paragraph_2_6")}
                 </h3>
                 <h3 style={paragraphTextNoMargin}>
                     {t("tutorial_4.paragraph_2_6_1")}
                 </h3>
+
                 <br/>
+                <div style={flexDivStyle}>
+                    <h3>
+                        {t("tutorial_4.trans1Assume_1")+"\u00a0"}
+                    </h3>
+                    <CustomTextField
+                        onChange = {
+                            (e) => {
+                                if (e.length === 0){
+                                    setTrans2Text1(0)
+                                } else {
+                                    setTrans2Text1(parseInt(e))
+                                }
+                            }
+                        }
+                    />
+                    <h3>
+                        {"\u00a0"+t("tutorial_4.dollars")}
+                    </h3>
+                </div>
+                <div style={flexDivStyle}>
+                    <h3>
+                        {t("tutorial_4.trans1Assume_3")+"\u00a0"}
+                    </h3>
+                    <CustomTextField
+                        onChange = {
+                            (e) => {
+                                if (e.length === 0){
+                                    setTrans2Text2(0)
+                                } else {
+                                    setTrans2Text2(parseInt(e)) 
+                                }
+                            }
+                        }
+                    />
+                    <h3>
+                        {"\u00a0"+t("tutorial_4.dollars")}
+                    </h3>
+                </div>
+
+                <div style={trans2DivStyle}>
+                    <h3 style={trans2text1Style}>
+                        {trans2Text1}
+                    </h3>
+                    <h3 style={trans2text2Style}>
+                        {trans2Text2}
+                    </h3>
+                    <h3 style={trans2text3Style}>
+                        {parseInt(trans2Text1)+parseInt(trans2Text2)}
+                    </h3>
+                </div>
+                <br/><br/><br/><br/><br/><br/>
+
                 <h2 style={subTitleText}>
                     {t("tutorial_4.subTitle_3")}
                 </h2>
