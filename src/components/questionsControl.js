f4e1a0040efb2c33ca4466069fd6122de2a9df5c
Martin Hui <huim13a@gmail.com>
2023-03-09 18:32:31 +0800

    update

--- src/components/questionsControl.js
+++ src/components/questionsControl.js
@@ -1,20 +1,52 @@
 import { useTranslation } from 'react-i18next';
 
-export const Questions = [
+export const QuestionsControl = [
     {
         question: "quiz.q1",
         choice1: "quiz.q1_c1",
         choice2: "quiz.q1_c2",
         choice3: "quiz.q1_c3",
         choice4: "quiz.q1_c4",
-        answer: "choice3",
+        answer: "choice2",
     },
     {
-        question: "quiz.q1",
+        question: "quiz.q2",
         choice1: "quiz.q2_c1",
         choice2: "quiz.q2_c2",
         choice3: "quiz.q2_c3",
         choice4: "quiz.q2_c4",
+        answer: "choice3",
+    },
+    {
+        question: "quiz.q3",
+        choice1: "quiz.q3_c1",
+        choice2: "quiz.q3_c2",
+        choice3: "quiz.q3_c3",
+        choice4: "quiz.q3_c4",
         answer: "choice4",
-    }
+    },
+    {
+        question: "quiz.q4",
+        choice1: "quiz.q4_c1",
+        choice2: "quiz.q4_c2",
+        choice3: "quiz.q4_c3",
+        choice4: "quiz.q4_c4",
+        answer: "choice1",
+    },
+    {
+        question: "quiz.q5",
+        choice1: "quiz.q5_c1",
+        choice2: "quiz.q5_c2",
+        choice3: "quiz.q5_c3",
+        choice4: "quiz.q5_c4",
+        answer: "choice2",
+    },
+    {
+        question: "quiz.q6",
+        choice1: "quiz.q6_c1",
+        choice2: "quiz.q6_c2",
+        choice3: "quiz.q6_c3",
+        choice4: "quiz.q6_c4",
+        answer: "choice4",
+    },
 ]
--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -1,7 +1,7 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
 import { useTranslation } from 'react-i18next';
 import { motion } from "framer-motion";
-import { Questions } from '../components/questionsControl.js'
+import { QuestionsControl } from '../components/questionsControl.js'
 import Button from '@mui/material/Button';
 
 export default function Test(){
@@ -24,10 +24,29 @@ export default function Test(){
         width: "275px",
         height: "100px"
     }
+    const nextButtonStyle={
+        width: "150px",
+        height: "50px"
+    }
     const centerDivStyle = {
         textAlign: "-webkit-center"
     }
-    const [currentQ, setCurrentQ] = useState(1)
+    const [currentQ, setCurrentQ] = useState(0)
+    const [currentA, setCurrentA] = useState("")
+    const [score, setScore] = useState(0)
+
+    const checkAnswer = () => {
+        if (QuestionsControl[currentQ].answer === currentA){
+            setScore(score+1)
+        }
+        setCurrentQ(currentQ+1)
+        setCurrentA("")
+    }
+    const restartQuiz = () => {
+        setCurrentQ(0)
+        setCurrentA("")
+        setScore(0)
+    }
 
     return(
         <motion.div
@@ -36,41 +55,83 @@ export default function Test(){
             exit={{ opacity: 0 }}
             style={divStyle}
             >
-            <h2 style={subTitleText}>
-                {t(Questions[currentQ].question)}
-            </h2>
-            <div style={centerDivStyle}>
-                <br/>
-                <div style={rowDiv}>
-                    <Button 
-                        color="secondary" 
-                        variant="contained" 
-                        style={buttonStyle}>
-                        <h4>{t(Questions[currentQ].choice1)}</h4>
-                    </Button>
-                    <Button 
-                        color="secondary" 
-                        variant="contained"
-                        style={buttonStyle}>
-                        <h4>{t(Questions[currentQ].choice2)}</h4>
-                    </Button>
+                {currentQ < 6
+                ?<div>
+                    <h2 style={subTitleText}>
+                        {t(QuestionsControl[currentQ].question)}
+                    </h2>
+                    <h1>{score}</h1>
+                    <div style={centerDivStyle}>
+                        <br/>
+                        <div style={rowDiv}>
+                            <Button 
+                                color="secondary" 
+                                variant="contained" 
+                                style={buttonStyle}
+                                onClick={()=>setCurrentA("choice1")}>
+                                <h4>{t(QuestionsControl[currentQ].choice1)}</h4>
+                            </Button>
+                            <Button 
+                                color="secondary" 
+                                variant="contained"
+                                style={buttonStyle}
+                                onClick={()=>setCurrentA("choice2")}>
+                                <h4>{t(QuestionsControl[currentQ].choice2)}</h4>
+                            </Button>
+                        </div>
+                        <br/><br/><br/>
+                        <div style={rowDiv}>
+                            <Button 
+                                color="secondary" 
+                                variant="contained"
+                                style={buttonStyle}
+                                onClick={()=>setCurrentA("choice3")}>
+                                <h4>{t(QuestionsControl[currentQ].choice3)}</h4>
+                            </Button>
+                            <Button 
+                                color="secondary" 
+                                variant="contained"
+                                style={buttonStyle}
+                                onClick={()=>setCurrentA("choice4")}>
+                                <h4>{t(QuestionsControl[currentQ].choice4)}</h4>
+                            </Button>
+                        </div>
+                        <br/><br/><br/>
+                        <div>
+                            <Button
+                                color="secondary" 
+                                variant="contained"
+                                style={nextButtonStyle}
+                                onClick={checkAnswer}>
+                                <h4>{t("quiz.next")}</h4>
+                            </Button>
+                        </div>
+                    </div>
                 </div>
-                <br/><br/><br/>
-                <div style={rowDiv}>
-                    <Button 
-                        color="secondary" 
-                        variant="contained"
-                        style={buttonStyle}>
-                        <h4>{t(Questions[currentQ].choice3)}</h4>
-                    </Button>
-                    <Button 
-                        color="secondary" 
-                        variant="contained"
-                        style={buttonStyle}>
-                        <h4>{t(Questions[currentQ].choice4)}</h4>
-                    </Button>
-                </div>
-            </div>
+                :<div>
+                    <div style={centerDivStyle}>
+                        <h2>
+                            {t("quiz.finalScore")+score}
+                        </h2>
+                        <div style={rowDiv}>
+                            <Button
+                                color="secondary" 
+                                variant="contained"
+                                style={nextButtonStyle}
+                                onClick={restartQuiz}>
+                                <h4>{t("quiz.restart")}</h4>
+                            </Button>
+                            <Button
+                                color="secondary" 
+                                variant="contained"
+                                style={nextButtonStyle}
+                                onClick={restartQuiz}>
+                                <h4>{t("quiz.checkAllAns")}</h4>
+                            </Button>
+                        </div>
+                    </div>
+                </div>}
+            
         </motion.div>
     )
 }
